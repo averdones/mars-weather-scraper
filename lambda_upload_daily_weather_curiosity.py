5752c2873836435069c90f9b06c02c93f0aa90ea
Antonio <averdones@gmail.com>
2023-02-14 16:37:24 -0500

    Remove AWS dependency

--- lambda_upload_daily_weather_curiosity.py
+++
@@ -1,7 +0,0 @@
-from src.upload_aws import upload_daily_weather
-from src.scraper import get_selenium_driver
-
-
-def lambda_handler(event, context):
-    driver = get_selenium_driver("/opt/chromedriver/chromedriver", "/opt/chromedriver/headless-chromium")
-    upload_daily_weather("mars-weather", driver)

bee26ddfb31bef50e95c2f9a46b2fdb18af8474a
Antonio <averdones@gmail.com>
2023-11-07 20:57:47 -0500

    Add more debugging prints

--- upload_daily_weather.py
+++ upload_daily_weather.py
@@ -16,42 +16,42 @@ def main():
     driver = get_selenium_driver()
     weather_day_data = download_weather_today(driver)
     parsed_weather_day_data = parse_weather_day(weather_day_data)
-
-    sol = parsed_weather_day_data["sol"]
-    if get_item(sol, DailyWeather) is not None:
-        print("Weather hasn't been updated in the webpage")
-        return
-    else:
-        # Check if they updated more than one day
-        n_last_days = 0
-        sol -= 1
-        while get_item(sol, DailyWeather) is None:
-            n_last_days += 1
-            sol -= 1
-
-        if n_last_days == 0:
-            # Upload only today's weather
-            try:
-                DailyWeather(**parsed_weather_day_data).save()
-            except PutError as e:
-                print(e)
-        else:
-            # Upload today's weather and the last n days
-            driver = get_selenium_driver()
-            weather_last_n_days_data = download_weather_last_n_days(driver, n_last_days)
-            for weather_data in weather_last_n_days_data:
-                parsed_weather_data = parse_weather_day(weather_data)
-                for attempt in range(10):
-                    try:
-                        DailyWeather(**parsed_weather_data).save()
-                    except PutError:
-                        print("Sleeping...")
-                        time.sleep(1)
-                    else:
-                        break
-                else:
-                    logger.error(f"Failed to upload data to DynamoDB. Data: {parsed_weather_data}")
-                    raise
+    #
+    # sol = parsed_weather_day_data["sol"]
+    # if get_item(sol, DailyWeather) is not None:
+    #     print("Weather hasn't been updated in the webpage")
+    #     return
+    # else:
+    #     # Check if they updated more than one day
+    #     n_last_days = 0
+    #     sol -= 1
+    #     while get_item(sol, DailyWeather) is None:
+    #         n_last_days += 1
+    #         sol -= 1
+    #
+    #     if n_last_days == 0:
+    #         # Upload only today's weather
+    #         try:
+    #             DailyWeather(**parsed_weather_day_data).save()
+    #         except PutError as e:
+    #             print(e)
+    #     else:
+    #         # Upload today's weather and the last n days
+    #         driver = get_selenium_driver()
+    #         weather_last_n_days_data = download_weather_last_n_days(driver, n_last_days)
+    #         for weather_data in weather_last_n_days_data:
+    #             parsed_weather_data = parse_weather_day(weather_data)
+    #             for attempt in range(10):
+    #                 try:
+    #                     DailyWeather(**parsed_weather_data).save()
+    #                 except PutError:
+    #                     print("Sleeping...")
+    #                     time.sleep(1)
+    #                 else:
+    #                     break
+    #             else:
+    #                 logger.error(f"Failed to upload data to DynamoDB. Data: {parsed_weather_data}")
+    #                 raise
 
 
 if __name__ == '__main__':
